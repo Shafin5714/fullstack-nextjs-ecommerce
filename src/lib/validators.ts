cf4bdddf04ac2e68af00f2efa18b36928dfe30b9
Shafin5714 <shafin5714@gmail.com>
2025-06-17 11:51:48 +0600

    Sign up error handle

--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -31,7 +31,7 @@ export const signInFormSchema = z.object({
 // Schema for signing up a user
 export const signUpFormSchema = z
   .object({
-    name: z.string().min(3, "Nam must be at least 3 characters"),
+    name: z.string().min(3, "Name must be at least 3 characters"),
     email: z.string().email("Invalid email address"),
     password: z.string().min(6, "Password must be at least 6 characters"),
     confirmPassword: z
